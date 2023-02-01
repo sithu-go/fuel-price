35aa9dba91ce18052d8d693e51dbfd7ae2b730c4
smith-golang <skkosmith@gmail.com>
2023-02-01 15:39:11 +0700

    add model and repo

--- cmd/handler/handler.go
+++ cmd/handler/handler.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"fuel-price/pkg/ds"
+	"fuel-price/pkg/repository"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	R    *gin.Engine
+	repo *repository.Repository
+}
+
+type HConfig struct {
+	R  *gin.Engine
+	DS *ds.DataSource
+}
+
+func NewHandler(c *HConfig) *Handler {
+	return &Handler{
+		R: c.R,
+		repo: repository.NewRepository(&repository.RepoConfig{
+			DS: c.DS,
+		}),
+	}
+}
+
+func (h *Handler) Register() {
+	// Dashboard
+	// dashboardHandler := NewDashboardHandler(h)
+	// dashboardHandler.register()
+
+}
